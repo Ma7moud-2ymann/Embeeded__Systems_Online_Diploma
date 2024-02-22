73e9e928401c3c53a2923bee3776a6b6804fd2b0
Mahmoud Ayman <160341295+Ma7moud-2ymann@users.noreply.github.com>
2024-02-22 22:38:04 +0200

    Add files via upload

---
+++ Unit_2_C_programming/assignments/EX5_C_Program_to_Find_ASCII_Value_of_a_Character.c
@@ -0,0 +1,22 @@
+
+/*
+ ======================================================================================================================
+Name        :unit2_less3_c_basic_assignment_ex5
+Author      : Mahmoud Ayman
+Created on	: Feb 22 2024
+Description : EX5: Write C Program to Find ASCII Value of a Character
+ ================== ====================================================================================================
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char c;
+    printf("enter yout char :");
+    scanf("%c",&c);
+    printf("Asci value is: %d",c);
+    return 0;
+}
