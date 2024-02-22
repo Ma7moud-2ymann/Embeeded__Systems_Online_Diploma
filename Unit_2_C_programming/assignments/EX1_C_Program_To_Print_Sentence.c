992bbf358527ca340eb17f22e794f4a9e0fdee13
Mahmoud Ayman <160341295+Ma7moud-2ymann@users.noreply.github.com>
2024-02-22 22:02:14 +0200

    Add files via upload

---
+++ Unit_2_C_programming/assignments/EX1_C_Program_To_Print_Sentence.c
@@ -0,0 +1,20 @@
+/****unit2_less3_c_basic_assignment_ex1*****/
+
+/*
+Write C Program to Print a Sentence
+
+i should see the Console as following:
+##########Console-output###
+C Programming
+###########################
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("C Programmming \n");
+    return 0;
+}
