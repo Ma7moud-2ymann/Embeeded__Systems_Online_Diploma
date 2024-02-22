e3148e1dfc4614bf8c920672f8c988bf84fb6f9c
Mahmoud Ayman <160341295+Ma7moud-2ymann@users.noreply.github.com>
2024-02-22 22:05:34 +0200

    Add files via upload

---
+++ Unit_2_C_programming/assignments/EX1_C_Program_To_Print_Sentence.c
@@ -0,0 +1,30 @@
+/****unit2_less3_c_basic_assignment_ex1*****/
+
+/*
+ ======================================================================================================================
+ Name        : EX1_C_Program_To_Print_Sentence.c
+ Author      : Mahmoud Ayman
+ Created on	 : Feb 22 2024
+ Description : Unit 2 C-Progtamming >> Assignment 1 C-Basic >> EX1 C Program To Print Sentence using Ansi-style (c99)
+ ================== ====================================================================================================
+ */
+
+
+/*
+Write C Program to Print a Sentence
+
+i should see the Console as following:
+##########Console-output###
+C Programming
+###########################
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("C Programmming \n");
+    return 0;
+}
